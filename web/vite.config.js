e3cd3aa02e94559dbd2b385489b9f8cd9e1d03d7
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-11-07 14:57:30 +0000

    Ensure source maps are not published in prod

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/static/dist/',
   plugins: [preact()],
   build: {
     outDir: './static/dist',
     assetsDir: '',
-    sourcemap: true,
+    sourcemap: mode !== 'production',
     manifest: true,
   },
-});
+}));
